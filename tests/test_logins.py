8cd50591e32ca79503b51aca6c55b85bb245be8c
erikjwagner <91382151+erikjwagner@users.noreply.github.com>
2023-09-23 13:24:18 -0400

    Update test_logins.py

--- tests/test_logins.py
+++ tests/test_logins.py
@@ -7,9 +7,9 @@ def test_login_success(request, controller):
     controller.home_page.login_button.click()
     controller.inventory_page.burger_menu.assert_displayed()
 
-# def test_login_failure(request, controller):
-#     """Incorrect Password doesn't allow login"""
-#     controller.home_page.user_name.send_keys("standard_user")
-#     controller.home_page.password.send_keys("invalid_password")
-#     controller.home_page.login_button.click()
-#     controller.inventory_page.burger_menu.assert_not_displayed()
+def test_login_failure(request, controller):
+    """Incorrect Password doesn't allow login"""
+    controller.home_page.user_name.send_keys("standard_user")
+    controller.home_page.password.send_keys("invalid_password")
+    controller.home_page.login_button.click()
+    controller.inventory_page.burger_menu.assert_not_displayed()
